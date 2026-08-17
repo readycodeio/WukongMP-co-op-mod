0135495ad067107a19a6e4314f7f37f111590d21
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2026-08-17 09:27:53 +0200

    Optimize inner query

--- WukongMp.Coop.Serverside/ScaleHpSystem.cs
+++ WukongMp.Coop.Serverside/ScaleHpSystem.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
-using ReadyM.Relay.Server.Sdk.Ecs;
+﻿using ReadyM.Relay.Server.Sdk.Ecs;
 using ReadyM.Relay.Server.Sdk.Ecs.Systems;
 using ReadyM.Wukong.Common.ECS.Components;
 
 namespace WukongMp.Coop.Serverside;
 
-public class ScaleHpSystem(EcsApi ecs, ILogger logger) : ModSystemBase
+public class ScaleHpSystem(EcsApi ecs) : ModSystemBase
 {
     public int ScalingPercent
     {
@@ -17,17 +16,13 @@ public class ScaleHpSystem(EcsApi ecs, ILogger logger) : ModSystemBase
     {
         // count all players in game, not just the area
         var players = 0;
-        ecs.Query<MainCharacterComponent, int>(ref players, static (ref _, ref p) =>
-        {
-            p++;
-        });
+        ecs.Query<MainCharacterComponent, int>(ref players, static (ref _, ref p) => { p++; });
 
         if (players == 0)
             return;
 
         var targetScalingPercent = ScalingPercent * players;
-
-        ecs.Query<TamerComponent, HpComponent>((ref tamer, ref hp) =>
+        ecs.Query<TamerComponent, HpComponent, int>(ref targetScalingPercent, static (ref tamer, ref hp, ref targetScalingPercent) =>
         {
             if (!tamer.IsBossOrElite)
                 return;
@@ -38,12 +33,10 @@ public class ScaleHpSystem(EcsApi ecs, ILogger logger) : ModSystemBase
             var currentMultPercent = hp.HpMaxMulPercent == 0 ? 100 : hp.HpMaxMulPercent;
             if (targetScalingPercent != currentMultPercent)
             {
-                var oldHp = hp.Hp;
                 var newHp = hp.Hp * ((float)targetScalingPercent / currentMultPercent);
 
                 hp.HpMaxMulPercent = targetScalingPercent;
                 hp.Hp = newHp;
-                logger.LogDebug("Set {Tamer} HP scaling from {OldMultiplier}% to {Multiplier}% for {Players} players (HP: {Old} -> {New})", tamer.Guid, currentMultPercent, targetScalingPercent, players, oldHp, newHp);
             }
         });
     }
